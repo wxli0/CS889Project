4d91ecbe7da17d82348f92dbf81ae51aa24f3148
EgeCiklabakkal <ege.cikla@gmail.com>
2021-07-23 13:22:40 -0400

    Bivariate coloring on taxi with random data

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.express as px
+import plotly.graph_objs as go
    
 map_data_path = "map_data/"
 covid_data_path = "covid_data/"
@@ -35,6 +36,49 @@ coviddf = pd.read_csv(covid_data_path + "covid_data-2020-3.csv")[:nzips]
 # zone overlap
 overlapdf = pd.read_csv(map_data_path + "taxi_zip_overlap.csv")
 
+# taxi bivariate color map
+nbiv_colors = 9
+biv_colors = [  "#e8e8e8", "#b5c0da", "#6c83b5",
+                "#b8d6be", "#90b2b3", "#567994",
+                "#73ae80", "#5a9178", "#2a5a5b"]
+bivcmap = {clr : clr for clr in biv_colors}
+
+# assign random colors to data
+bivcolors = np.array([biv_colors[i % nbiv_colors] for i in range(len(taxidf))])
+taxidf["bivcolor"] = bivcolors
+
+def colors_to_colorscale(biv_colors):
+    # biv_colors: list of n**2 color codes in hexa or RGB255
+    # returns a discrete colorscale  defined by biv_colors
+    n = len(biv_colors)
+    biv_colorscale = []
+    for k, col in enumerate(biv_colors):
+        biv_colorscale.extend([[round(k/n, 2) , col], [round((k+1)/n, 2), col]])
+    return biv_colorscale
+
+def colorsquare(text_x, text_y, colorscale, n=3):
+    # text_x : list of n strings, representing intervals of values for the
+    #   first variable or its n percentiles
+    # text_y : list of n strings, representing intervals of values for the
+    #   second variable or its n percentiles
+    # colorscale: Plotly bivariate colorscale
+    # returns the colorsquare as alegend for the bivariate choropleth, heatmap and more
+
+    z = [[j+n*i for j in range(n)] for i in range(n)]
+    n = len(text_x)
+    if len(text_x) != n   or len(text_y) != n  or len(colorscale) != 2*n**2:
+        raise ValueError(
+            'Your lists of strings  must have the length {n} and the colorscale, {n**2}')
+
+    text = [[text_x[j]+'<br>'+text_y[i] for j in range(len(text_x))] for i in range(len(text_y))]
+    return go.Heatmap(x=list(range(n)),
+                      y=list(range(n)),
+                      z=z,
+                      text=text,
+                      hoverinfo='text',
+                      colorscale=colorscale,
+                      showscale=False)
+
 # dash app
 app = dash.Dash(
     __name__,
@@ -46,8 +90,8 @@ app = dash.Dash(
 # figures
 choroplethHeight = 600
 taxifig = px.choropleth(taxidf, geojson=taxigj,
-                            locations="PULocationID", color="yellow_log_total_amount",
-                            color_continuous_scale="Viridis",
+                            locations="PULocationID", color="bivcolor",
+                            color_discrete_map=bivcmap,
                             hover_name="Zone",
                             hover_data={
                                 "yellow_total_amount": ":.2f",
@@ -55,8 +99,20 @@ taxifig = px.choropleth(taxidf, geojson=taxigj,
                                 "Borough" : True,
                                 "service_zone" : True,
                                 "yellow_log_total_amount" : False,
-                                "green_log_total_amount" : False},
+                                "green_log_total_amount" : False,
+                                "bivcolor" : False},
                             featureidkey="properties.LocationID", projection="mercator")
+
+# bivariate legend
+legendHeight = legendWidth = 300
+text_x = ['p_ratio<P_33', 'P_33<=p_ratio<=P_66', 'p_ratio>P_66']
+text_y = ['h_ratio<P_33', 'P_33<=h_ratio<=P_66', 'h_ratio>P_66']
+legend_axis = dict(showline=False, zeroline=False, showgrid=False,  ticks='', showticklabels=False)
+taxilegend = go.Figure(
+                data=colorsquare(text_x, text_y, colors_to_colorscale(biv_colors)),
+                layout=dict(xaxis=dict(legend_axis, title="P_33, P66 are percentiles", side="top"),
+                            yaxis=legend_axis,
+                            height=legendHeight, width=legendWidth))
 
 covidfig = px.choropleth(coviddf, geojson=covidgj,
                             locations="zip_code", color="hospitalization_rate",
@@ -87,14 +143,14 @@ def get_highlights(selections, geojson, lookup_dict):
 
 def get_taxifig(selectedLocs):
     # clear traces
-    taxifig.data = [taxifig.data[0]]
+    taxifig.data = taxifig.data[:nbiv_colors]
 
     if (len(selectedLocs) > 0):
         highlights = get_highlights(selectedLocs, taxigj, taxi_lookup)
         taxiHighlights = px.choropleth(taxidf.loc[taxidf["PULocationID"].isin(selectedLocs)],
                                         geojson=highlights,
-                                        locations="PULocationID", color="yellow_log_total_amount",
-                                        color_continuous_scale="Viridis",
+                                        locations="PULocationID", color="bivcolor",
+                                        color_discrete_map=bivcmap,
                                         hover_name="Zone",
                                         hover_data={
                                             "yellow_total_amount": ":.2f",
@@ -102,14 +158,18 @@ def get_taxifig(selectedLocs):
                                             "Borough" : True,
                                             "service_zone" : True,
                                             "yellow_log_total_amount" : False,
-                                            "green_log_total_amount" : False},
+                                            "green_log_total_amount" : False,
+                                            "bivcolor" : False},
                                         featureidkey="properties.LocationID",
                                         projection="mercator")
         taxiHighlights.update_traces(marker_line=dict(color="red", width=5))
         taxifig.add_trace(taxiHighlights.data[0])
 
     taxifig.update_geos(fitbounds="locations", visible=False)
-    taxifig.update_layout(margin={"r":0,"t":0,"l":0,"b":0}, height=choroplethHeight)
+    taxifig.update_layout(
+        margin={"r":0,"t":0,"l":0,"b":0},
+        height=choroplethHeight,
+        showlegend=False)
 
     return taxifig
             
@@ -141,14 +201,17 @@ def get_covidfig(selectedZips):
 app.layout = html.Div([
     html.Div([ 
         html.Div([
-            dcc.Graph(id="covid-choropleth",
-            figure=covidfig)
-        ], className="six columns"),
+            dcc.Graph(id="covid-choropleth")
+        ], className="five columns"),
 
         html.Div([
-            dcc.Graph(id="taxi-choropleth",
-            figure=taxifig)
-        ], className="six columns"),
+            dcc.Graph(id="taxi-choropleth")
+        ], className="five columns"),
+
+        html.Div([
+            dcc.Graph(id="taxi-legend",
+            figure=taxilegend)
+        ], className="one column")
     ], className="row"),
 
     html.Div([ 
@@ -214,4 +277,4 @@ def update_plots(covidClickData, taxiClickData, currentVisibility):
 
 # main
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run_server(debug=False)
